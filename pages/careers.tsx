a65826f3c5de5801e075d8a9a21b2d35f3fda1f0
cesar <cesar.hertz@icloud.com>
2023-04-03 10:41:02 +0800

    feat/career page draft

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -1,47 +1,65 @@
-import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
-import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
-     
 
 export default function Careers() {
   const { t } = useTranslation("common");
 
   return (
     <Layout>
-        <div className="flex flex-col pt-5">
-          <p className="text-3xl font-medium pt-3">{t("home.weDesign")}</p>
-          <p className="py-3 1 text-7xl font-extrabold">
-            {t("home.theathersForLife")}
-          </p>
-          <Image
-            className="basis-1/12"
-            src="/designBg.jpg"
-            alt="designBackground"
-            width={1200}
-            height={200}
-            priority
-          />
-        </div>
-        <div className="flex flex-col p-3">
-          <p className="py-3 text-xl w-6/12 font-thin uppercase">
-            {t("home.passionateCollective")}
-          </p>
-          <p className="text-lg w-8/12 leading-8 font-thin text-justify">
-            {t("home.passionateCollectiveTxt")}{" "}
-            {t("home.passionateCollectiveTxt")}
-          </p>
-        </div>
+      <div className="flex flex-col pt-5 mx-auto">
+        <Image
+          className=""
+          src="/designBg.jpg"
+          alt="designBackground"
+          width={900}
+          height={200}
+          priority
+        />
+      </div>
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.intro")}
+      </p>
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.email")}
+      </p>
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.emailAddress")}
+      </p>
 
-</Layout>
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.browse")}
+      </p>
 
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.positionTitleOne")}
+      </p>
+
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.positionDescriptionOne")}
+      </p>
+      
+      
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.positionTitleTwo")}
+      </p>
+
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.positionDescriptionTwo")}
+      </p>
+      
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.moreInfo")}
+      </p>
+
+      <p className="py-3 text-lg mx-auto text-center w-8/12 font-thin">
+        {t("careers.linkedin")}
+      </p>
+    </Layout>
   );
 }
 
--- pages/news.tsx
+++ pages/news.tsx
@@ -15,30 +15,10 @@ export default function News() {
 
   return (
     <Layout>
-        <div className="flex flex-col pt-5">
-          <p className="text-3xl font-medium pt-3">{t("home.weDesign")}</p>
-          <p className="py-3 1 text-7xl font-extrabold">
-            {t("home.theathersForLife")}
-          </p>
-          <Image
-            className="basis-1/12"
-            src="/designBg.jpg"
-            alt="designBackground"
-            width={1200}
-            height={200}
-            priority
-          />
-        </div>
-        <div className="flex flex-col p-3">
-          <p className="py-3 text-xl w-6/12 font-thin uppercase">
-            {t("home.passionateCollective")}
-          </p>
-          <p className="text-lg w-8/12 leading-8 font-thin text-justify">
-            {t("home.passionateCollectiveTxt")}{" "}
-            {t("home.passionateCollectiveTxt")}
-          </p>
-        </div>
-        </Layout>
+      <div className="flex flex-col pt-5">
+        <p className="text-3xl font-medium pt-3">News</p>
+      </div>
+    </Layout>
   );
 }
 
